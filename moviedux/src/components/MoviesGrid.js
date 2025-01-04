5e9eae62aad4e1c8f4312966f2042776ba1565eb
Anan328 <ananshah328@gmail.com>
2025-01-04 18:58:24 +0530

    added search bar

--- moviedux/src/components/MoviesGrid.js
+++ moviedux/src/components/MoviesGrid.js
@@ -4,6 +4,9 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
 
   useEffect(() => {
     fetch("movies.json")
@@ -14,12 +17,34 @@ export default function MoviesGrid() {
         setMovies(data);
       });
   }, []);
+  const handleSearchChange = (e)=> {
+    setSearchTerm(e.target.value);
+    
+  } 
 
+  const filteredMovies = movies.filter(movie =>
+    movie.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+  )
+  
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <MovieCard movie={movie} key={movie.id} />
-      ))}
+    <div>
+      <input
+        type="text"
+        placeholder="Search movies..."
+        className="search-input"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
+     <div className="filter-bar">
+
+     </div>
+
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
     </div>
   );
 }
