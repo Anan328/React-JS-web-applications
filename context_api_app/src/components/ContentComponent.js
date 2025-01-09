c399538b3df16ea786423a0d75267d057cfd6156
Anan328 <ananshah328@gmail.com>
2025-01-09 14:03:13 +0530

    changes

--- context_api_app/src/components/ContentComponent.js
+++ context_api_app/src/components/ContentComponent.js
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext,useEffect } from 'react'
 import ThemeContext from '../context/ThemeContext'
+import '../style.css';
 
 export default function ContentComponent() {
     const {theme, toggleTheme} = useContext(ThemeContext);
+
+    useEffect(() => {
+        document.body.className = theme === 'light' ? 'light-theme' : 'dark-theme';
+    }, [theme]);
+
   return (
     <div>
         Current Theme: {theme}
         <br/>
-        <button onClick={()=>toggleTheme()}>Toggle Theme</button>
+        <button onClick={toggleTheme}>Toggle Theme</button>
 
     </div>
   )
