fc67f0de1a1f3a394c9555c5332bd7dbd0a106c7
Anan328 <ananshah328@gmail.com>
2025-01-11 00:50:08 +0530

    private Route

--- book-library/src/components/PrivateRoute.js
+++ book-library/src/components/PrivateRoute.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Navigate} from "react-router-dom";
 
 const PrivateRoute = ({Component})=>{
-    const isAuthenticated = false;
+    const isAuthenticated = false; // backend will provide this token by sending a fetch req to it
 
     return isAuthenticated? Component : <Navigate to="/"/>
 }
