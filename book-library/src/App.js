a8a90e128deb9346b982ac408a63e495474b004a
Anan328 <ananshah328@gmail.com>
2025-01-10 20:59:17 +0530

    router

--- book-library/src/App.js
+++ book-library/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import BooksContextProvider from './context/BooksContextProvider';
 import Books from './components/Books';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <BooksContextProvider>
-      <div className="App">
-      
-      <Books/>
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Books/>}></Route>
+        </Routes>
+      </Router>
     </BooksContextProvider>
   );
 }
