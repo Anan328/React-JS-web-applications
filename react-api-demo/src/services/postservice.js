13b516b1df85375f3d0e649511b3bb0481ea6fbb
ANAN <ananshah328@gmail.com>
2025-01-16 16:41:05 +0530

    Update postservice.js

--- react-api-demo/src/services/postservice.js
+++ react-api-demo/src/services/postservice.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // service layer
 const api  = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL
+    baseURL: "https://jsonplaceholder.typicode.com"
 })
 
 const getPosts = () => api.get("/posts");
